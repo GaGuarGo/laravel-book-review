c93bf4f1f4829c168407eadc9655c321be0490a3
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-05 20:50:19 -0300

    Local Query Scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,13 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 class Book extends Model
 {
     use HasFactory;
 
 
-    public function reviews(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+
+    }
+
 }
