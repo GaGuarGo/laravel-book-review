4fd6f0f19a0ecd4b67515e69db5ba861775baa10
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-06 20:59:51 -0300

    Scoped Resource Controller - Adding a Review

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -18,6 +18,12 @@
         </div>
     </div>
 
+    <div class="mb-4">
+        <a class="btn" href="{{route('books.review.create', $book)}}">
+            Add a Review
+        </a>
+    </div>
+
     <div>
         <h2 class="mb-4 text-xl font-semibold">Reviews</h2>
         <ul>
