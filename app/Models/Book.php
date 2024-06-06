3bf901d51617539c8a13d66ff19b98dddeecb078
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-06 20:30:13 -0300

    Rating and Review Count on All Pages

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -24,16 +24,30 @@ class Book extends Model
 
     }
 
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ]);
+    }
+
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating');
+    }
+
     public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withCount(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)])
+        return $query->withReviewsCount()
             ->orderBy('reviews_count', 'desc');
     }
 
     public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withAvg(['reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)],
-            'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvgRating()
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 
     public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
@@ -41,7 +55,7 @@ class Book extends Model
         return $query->having('reviews_count', '>=', $minReviews);
     }
 
-    private function dateRangeFilter(Builder $query, string $from, string $to)
+    private function dateRangeFilter(Builder $query, ?string $from, ?string $to)
     {
 
         if ($from && !$to) {
@@ -50,6 +64,8 @@ class Book extends Model
             $query->where('created_at', '<=', $to);
         } elseif ($from && $to) {
             $query->wherebetween('created_at', [$from, $to]);
+        } else {
+            $query->latest();
         }
 
     }
@@ -72,6 +88,13 @@ class Book extends Model
     public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
     {
         return $query->highestRated(now()->subMonths(6), now())->popular(now()->subMonths(6), now())->minReviews(2);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn(Book $book) => cache()->forget('book:' . $book->id));
+        static::deleted(fn(Book $book) => cache()->forget('book:' . $book->id));
+
     }
 }
 
