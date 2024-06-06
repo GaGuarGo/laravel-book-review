6dc4e7eef58f5d1398c709ee8c6c506caa4dbea2
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-06 19:41:35 -0300

    One Book Page

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -49,9 +49,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+
+        //lazy loading = false, carregando a relação entre models logo quando o
+        //endpoint é chamado
+        return view('books.show', ['book' => $book->load(['reviews' => fn($query) => $query->latest()
+                ]
+            )
+            ]
+        );
     }
 
     /**
