9e4188592ad3d88b5f86af6954b5c67b0471cbca
GABRIEL GUARNIERI GOMES <gabrielguarnierigomes@gmail.com>
2024-06-06 20:09:05 -0300

    Invalidating Cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -54,13 +54,13 @@ class BookController extends Controller
     public function show(Book $book)
     {
 
+        $cacheKey =  d;
+
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load(['reviews' => fn($query) => $query->latest()]));
+
         //lazy loading = false, carregando a relação entre models logo quando o
         //endpoint é chamado
-        return view('books.show', ['book' => $book->load(['reviews' => fn($query) => $query->latest()
-                ]
-            )
-            ]
-        );
+        return view('books.show', ['book' => $book]);
     }
 
     /**
